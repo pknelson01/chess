1bd0a4c06e3e1c5f244f3229d3d0fa1fbad75e52
pknelson01 <pk.elliott11@gmail.com>
2026-07-08 23:24:15 -0600

    phase-1 complete

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -2,6 +2,7 @@ package chess;
 
 import java.util.Collection;
 import java.util.ArrayList;
+import java.util.Objects;
 
 /**
  * A class that can manage a chess game, making moves on a board
@@ -258,4 +259,17 @@ public class ChessGame {
      * @return the chessboard
      */
     public ChessBoard getBoard() { return board; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof ChessGame chessGame)) {
+            return false;
+        }
+        return Objects.equals(board, chessGame.board) && teamTurn == chessGame.teamTurn;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(board, teamTurn);
+    }
 }

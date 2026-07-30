022bcbef279ebfc49202cb331b8bad039b38563d
Parker Nelson <pk.elliott11@gmail.com>
2026-07-29 20:42:00 -0600

    routine push

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -5,6 +5,12 @@ import chess.*;
 public class ClientMain {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        String login = "Login as an existing user: \"l\", \"login\" <USERNAME> <PASSWORD>";
+        String register = "Register a new user: \"r\", \"register\" <USERNAME> <PASSWORD> <EMAIL>";
+        String exit = "Exit the program: \"q\", \"quit\"";
+        String help = "Print this message: \"h\", \"help\"";
+        // System.out.println("♕ 240 Chess Client: " + piece);
+
+        System.out.printf(" ♕ Welcome to Parker's Chess Application. Sign in to start. ♕\nOptions:\n%s\n%s\n%s\n%s", login, register, exit, help);
     }
 }
